fb14764cc9b5ff6a4efe73dbbb8fa76f13aa5114
SophiaL1024 <sophiafengliu@gmail.com>
2021-06-23 18:46:11 +0000

    add comments

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,11 +4,11 @@ class Admin::DashboardController < ApplicationController
 
   def show
     @categories=Category.all
-
+    # define an array to store product quantity for each category
     count_products_by_category=@categories.map do |category|
       Product.where(category_id:category.id).count      
     end
-    
+
     render locals: {
       count_products_by_category:count_products_by_category
     }
