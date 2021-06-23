473bba8b83a778dc4d246db64bb9297d037fb772
SophiaL1024 <sophiafengliu@gmail.com>
2021-06-23 18:35:31 +0000

    add product quantity and links

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,7 +3,16 @@ class Admin::DashboardController < ApplicationController
     http_basic_authenticate_with name:ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']  
 
   def show
-    @products = Product.all
     @categories=Category.all
+
+    count_products_by_category=@categories.map do |category|
+      Product.where(category_id:category.id).count      
+    end
+    
+    render locals: {
+      count_products_by_category:count_products_by_category
+    }
+
   end
+
 end
