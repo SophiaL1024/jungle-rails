ca9b5f6276f5672aa2ae64a7fe7bfc6b1896bca8
SophiaL1024 <sophiafengliu@gmail.com>
2021-06-24 04:12:46 +0000

    add password confirmation check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,18 +3,24 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
+
+    if user_params[:password]==user_params[:password_confirmation]
+      user = User.new(user_params)
+        if user.save
+           session[:user_id] = user.id
+           redirect_to root_path
+        else
+          redirect_to new_user_path, alert: user.errors.full_messages.first 
+        end
     else
-      redirect_to '/users/new'
+        redirect_to new_user_path, alert:"password wrong" 
     end
+ 
   end  
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password,:password_confirmation)
   end
 
 end
