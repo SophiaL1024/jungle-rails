8f00c6ae2dc28f0f927e96a2751f3a4780756b74
SophiaL1024 <sophiafengliu@gmail.com>
2021-06-29 21:01:10 +0000

    add feature spec for homepage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require "capybara/rails"
-require "capybara/rspec"
-require "capybara/poltergeist"
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
@@ -11,6 +8,9 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require "capybara/rspec"
+require "capybara/poltergeist"
+require "capybara/rails"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
