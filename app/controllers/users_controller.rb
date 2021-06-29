1b10b6d1edf47fe2ade1ed00b96fd5562cbfe061
SophiaL1024 <sophiafengliu@gmail.com>
2021-06-29 03:42:16 +0000

    add user definition validation and test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,20 @@ class UsersController < ApplicationController
 
   def create
 
-    if user_params[:password]==user_params[:password_confirmation]
-      user = User.new(user_params)
-        if user.save
-           session[:user_id] = user.id
-           redirect_to root_path
-        else
-          redirect_to new_user_path, alert: user.errors.full_messages.first 
-        end
+    if user_params[:password].empty? 
+      redirect_to new_user_path, alert:"Password should not be blank."       
+    elsif user_params[:password]!=user_params[:password_confirmation]
+      redirect_to new_user_path, alert:"Password do not match." 
+    elsif User.find_by email:user_params[:email] 
+      redirect_to new_user_path, alert:"The email already exists."       
     else
-        redirect_to new_user_path, alert:"password do not match." 
+      user = User.new(user_params)
+      if user.save
+         session[:user_id] = user.id
+         redirect_to root_path
+      else
+        redirect_to new_user_path, alert: user.errors.full_messages.first 
+      end
     end
  
   end  
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  before(:each) do
+    @user_params={
+      first_name: 'Sophia',
+      last_name: 'Liu',
+      email: 'sophialiu@example.com',
+      password: '123456',
+      password_confirmation: '123456'
+    }
+    
+  end
+
+  context "validation specs" do
+    it" should have password" do
+      @user_params[:password]=""
+      @user = User.create @user_params
+      expect(@user).not_to be_valid 
+      expect(@user.errors.full_messages.first).to eq("Password can't be blank")     
+    end
+
+    it" password and password_confirmation should match " do
+      @user_params[:password_confirmation]="12345"
+      @user = User.create @user_params
+      expect(@user).not_to be_valid   
+      expect(@user.errors.full_messages.first).to eq("Password confirmation doesn't match Password")    
+    end
+
+    it "email should be unique and not case sensitive" do
+      @user = User.create @user_params
+      @user_params[:email]="Sophialiu@example.com"
+      @user = User.create @user_params
+      expect(@user).not_to be_valid   
+      expect(@user.errors.full_messages.first).to eq("Email has already been taken")    
+    end
+   
+    it "email should be required" do
+
+    end
+
+    it "first_name should be required" do
+
+    end
+
+    it "last_name should be required" do
+
+    end
+
+  end
+end
